b810b31d72b1ae1c9890234aadb08ef1875582f0
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-11-02 21:49:02 +0100

    change mongo uri

--- app/database/config-mongodb.js
+++ app/database/config-mongodb.js
@@ -1,8 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
-const uri = "mongodb+srv://"+process.env.MONGO_USER+":"+process.env.MONGO_PASSWORD+"@"+process.env.MONGO_HOST+"/"+process.env.MONGO_NAME+"?retryWrites=true&w=majority";
-
+const uri=process.env.MONGO_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 module.exports= {client}
